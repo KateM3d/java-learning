9f8a6203768d146a630ee92eac4a4cb3a8ebac43
KateM3d <ekaterina8medvedeva@gmail.com>
2022-10-06 17:32:34 -0400

    practice classes and constructor

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+public class Car {
+    String color;
+    String made;
+
+    public Car() {
+
+    }
+
+    public Car(String made, String color) {
+        this.color = color;
+        this.made = made;
+    }
+
+    static String stop(String stopMessage) {
+        return stopMessage;
+    }
+
+    static void newStop() {
+        System.out.print(" make new stop ");
+    }
+
+    ;
+
+    static void newGo() {
+        System.out.print(" you can go faster!");
+    }
+
+    static String proceed(String proceedMessage) {
+        return proceedMessage;
+    }
+
+    public static void main(String[] args) {
+        Car toyota = new Car("Toyota", "white");
+        Car hyndai = new Car("Hyndai", "blue");
+        Car bmw = new Car();
+        bmw.color = "green";
+        bmw.made = "bmw";
+
+        System.out.println(toyota.made + " with the color " + toyota.color);
+        System.out.println(hyndai.made + " with the color " + hyndai.color);
+        System.out.println(toyota.made + " please " + toyota.stop("stop at the red light"));
+        System.out.println(hyndai.made + " please " + hyndai.proceed("proceed at the green light"));
+        System.out.println(bmw.made + " created with the other constructor " + " color " + bmw.color + " ");
+        bmw.newStop();
+        bmw.newGo();
+    }
+
+}
--- src/Programmer.java
+++ src/Programmer.java
@@ -1,18 +1,31 @@
-public class Programmer{
+public class Programmer {
     String name;
     int age;
+    static int count = 0;
 
-    public static void main (String[] args){
-        Programmer anna = new Programmer();
-        anna.name="Anna";
-        anna.age=12;
+    public Programmer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-        Programmer andrew=new Programmer();
-        andrew.name="Andrew";
-        andrew.age=27;
+    public String write(String writeType) {
+        return " writes " + writeType;
+    }
+
+    public String drink(String drinkType) {
+        return " drinks " + drinkType;
+    }
+
+    public static void main(String[] args) {
+        Programmer anna = new Programmer("Anna", 25);
+
+        Programmer andrew = new Programmer("Andrew", 27);
 
         System.out.println("Her name is " + anna.name);
         System.out.println("His name is " + andrew.name);
+        System.out.println(count + " total programmers");
+        System.out.println("Today " + anna.name + anna.write("code"));
+        System.out.println("but " + andrew.name + andrew.drink("coffee"));
     }
 }
 
