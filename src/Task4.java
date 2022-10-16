10f96e7cb283929f8e3f5743ea43305b5be2d733
KateM3d <ekaterina8medvedeva@gmail.com>
2022-10-16 11:19:29 -0400

    task 4

---
+++ src/Task4.java
@@ -0,0 +1,8 @@
+public class Task4 {
+    public static void main(String[] args) {
+        String text = "Training Java";
+
+        System.out.println(text.length() + " " + text.charAt(5) + " October 16th 2022 ");
+
+    }
+}
