be984c810723527797b19e02e9b57a240f281cc3
KateM3d <ekaterina8medvedeva@gmail.com>
2022-10-23 20:50:50 -0400

    practice 1

--- src/week5/Practice2.java
+++ src/week5/Practice2.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Practice2 {
     public static void main(String[] args) {
-        System.out.println("please enter a number>>");
+        System.out.println("please enter an integer>>");
         Scanner inputInt = new Scanner(System.in);
         int n = inputInt.nextInt();
 
-        System.out.println(n%2==0 ? "Even" : "Odd");
+        System.out.println(n % 2 == 0 ? "Even" : "Odd");
     }
 }
