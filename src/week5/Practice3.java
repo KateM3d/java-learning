6d5bec8ffc8514225f8eeda60284526dbbd7842c
KateM3d <ekaterina8medvedeva@gmail.com>
2022-10-23 21:01:59 -0400

    practice 3

--- src/week5/Practice3.java
+++ src/week5/Practice3.java
@@ -1,4 +1,24 @@
 package week5;
 
 public class Practice3 {
+    enum Week {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+
+    ;
+
+    public static void main(String[] args) {
+        Week i = Week.SATURDAY;
+        switch (i) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+            case FRIDAY:
+                System.out.println("It's a week day!");
+                break;
+            case SATURDAY:
+            case SUNDAY:
+                System.out.println("It's the weekend!");
+                break;
+        }
+    }
 }
