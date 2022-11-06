edec14171ab59639e4724531219126177a075f77
KateM3d <ekaterina8medvedeva@gmail.com>
2022-11-05 22:32:22 -0400

    add practice1

---
+++ src/week7/Practice1.java
@@ -0,0 +1,16 @@
+package week7;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        //Создайте массив из целых чисел, выведете его на экран и посчитайте для него среднее арифметическое. (Среднее
+        //арифметическое равно сумме элементов, разделенной на их количество.)
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+
+        for (int el : numbers) {
+            sum = sum + el;
+        }
+        System.out.println(sum / numbers.length);
+    }
+}
